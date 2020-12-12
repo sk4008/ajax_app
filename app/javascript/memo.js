5c824676d4179625f91d7c5e90c72bd7d51dcf80
sk4008 <73086120+sk4008@users.noreply.github.com>
2020-12-12 15:57:39 +0900

    修正点

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,8 +1,6 @@
-<<<<<<< Updated upstream
-=======
 function memo() {
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {});
+  submit.addEventListener("click", (e) => {
   const formData = new FormData(document.getElementById("form"));
   const XHR = new XMLHttpRequest();
   XHR.open("POST", "/posts", true);
@@ -32,4 +30,3 @@ function memo() {
 });
 }
 window.addEventListener("load", memo);
->>>>>>> Stashed changes
