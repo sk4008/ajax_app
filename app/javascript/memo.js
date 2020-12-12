3db8b2e905c131b6963179cfb58f45655d987e87
sk4008 <73086120+sk4008@users.noreply.github.com>
2020-12-12 15:49:15 +0900

    修正点

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,6 +1,6 @@
 function memo() {
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", (e) => {});
+  submit.addEventListener("click", (e) => {
   const formData = new FormData(document.getElementById("form"));
   const XHR = new XMLHttpRequest();
   XHR.open("POST", "/posts", true);
